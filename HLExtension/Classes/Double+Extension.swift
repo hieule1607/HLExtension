491323c722b82263e3bbf56b67ef678dfc52510a
Hieu Le <hieule1607@gmail.com>
2018-10-29 23:28:30 +0700

    fix public

--- HLExtension/Classes/Double+Extension.swift
+++ HLExtension/Classes/Double+Extension.swift
@@ -5,7 +5,7 @@
 //  Created by Hieu Lam on 10/29/18.
 //
 
-extension Double {
+public extension Double {
     var clean: String {
         return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
     }
