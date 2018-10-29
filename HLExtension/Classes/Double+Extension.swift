fbc4e03dce2f43dd98258eecf120da1568722580
Hieu Le <hieule1607@gmail.com>
2018-10-29 23:23:57 +0700

    Add Double extension

---
+++ HLExtension/Classes/Double+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Double+Extension.swift
+//  HLExtension
+//
+//  Created by Hieu Lam on 10/29/18.
+//
+
+extension Double {
+    var clean: String {
+        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+    }
+    
+}
