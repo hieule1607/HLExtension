a4eb994dd828aebce95ffd71c04cccba99bdc056
Hieu Le <hieule1607@gmail.com>
2018-10-30 00:01:00 +0700

    add FlowLayout

---
+++ HLExtension/Classes/CollectionViewFlowlayout.swift
@@ -0,0 +1,27 @@
+//
+//  CollectionViewFlowlayout.swift
+//  HLExtension
+//
+//  Created by Hieu Lam on 10/29/18.
+//
+
+public class FlowLayout: UICollectionViewFlowLayout {
+    override public func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let attributes = super.layoutAttributesForElements(in: rect)
+        
+        var leftMargin = sectionInset.left
+        var maxY: CGFloat = -1.0
+        attributes?.forEach { layoutAttribute in
+            if layoutAttribute.frame.origin.y >= maxY {
+                leftMargin = sectionInset.left
+            }
+            
+            layoutAttribute.frame.origin.x = leftMargin
+            
+            leftMargin += layoutAttribute.frame.width + minimumInteritemSpacing
+            maxY = max(layoutAttribute.frame.maxY , maxY)
+        }
+        
+        return attributes
+    }
+}
