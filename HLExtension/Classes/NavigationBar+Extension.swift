5df0393e971b4c091f01ed7061dcc7fc825a9129
Hieu Le <hieule1607@gmail.com>
2018-11-27 09:46:19 +0700

    Navigation Extension

---
+++ HLExtension/Classes/NavigationBar+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationBar+Extension.swift
+//  HLExtension
+//
+//  Created by Hieu Lam on 11/27/18.
+//
+
+public extension UINavigationBar {
+    
+    public func shouldRemoveShadow(_ value: Bool) -> Void {
+        if value {
+            self.setValue(true, forKey: "hidesShadow")
+        } else {
+            self.setValue(false, forKey: "hidesShadow")
+        }
+    }
+}
