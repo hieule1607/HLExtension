4178dd2fb672c658f85de1728eb02d125e91768e
Hieu Le <hieule1607@gmail.com>
2018-11-28 17:22:07 +0700

    Add UILabel edit

--- HLExtension/Classes/Label+Extension.swift
+++ HLExtension/Classes/Label+Extension.swift
@@ -6,7 +6,7 @@
 //
 
 public extension UILabel {
-    func halfTextColorChange (fullText : String , changeText : String, attributeDict: [NSAttributedStringKey:Any] ) {
+    func highlightSubString (fullText : String , changeText : String, attributeDict: [NSAttributedStringKey:Any] ) {
         let strNumber: NSString = fullText as NSString
         let range = (strNumber).range(of: changeText)
         
